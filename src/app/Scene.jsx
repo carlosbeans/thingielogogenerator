a53bfd7d7ef49da5fa445485db161f8865a164b3
Ben <tryptefan@gmail.com>
2025-08-14 17:59:41 -0600

    got the godrays going through the svg

--- src/app/Scene.jsx
+++ src/app/Scene.jsx
@@ -9,47 +9,75 @@ import { useRef } from 'react'
 import { BlendFunction } from 'postprocessing'
 import { Circle } from '@react-three/drei'
 import { KernelSize } from 'postprocessing'
+import { Resizer } from 'postprocessing'
+import { MeshTransmissionMaterial } from '@react-three/drei'
+import { useLoader } from '@react-three/fiber'
+import * as THREE from 'three'
 
 
 export default function Scene() {
 
+  const svgTexture = useLoader(THREE.TextureLoader, '/logo.svg')
+
+  // inverted shader
+  const invertedAlphaMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    map: { value: svgTexture },
+    color: { value: new THREE.Color('black') }
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+    fragmentShader: `
+    uniform sampler2D map;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      vec4 texColor = texture2D(map, vUv);
+      float invertedAlpha = 1.0 - texColor.a;
+
+      // ✨ FIX: If the pixel is supposed to be see-through,
+      // discard it entirely so it doesn't block the god rays.
+      if (invertedAlpha < 0.5) {
+        discard;
+      }
+
+      gl_FragColor = vec4(color, invertedAlpha);
+    }
+  `,
+  transparent: true
+})
+
 
 
   const lightRef = useRef()
-  
+
   const particles = useMemo(() => {
-      const temp = []
-      for (let i = 0; i < 300; i++) {
-        // Generate x, y, z coordinates (3 values per particle)
-        temp.push((Math.random() - 0.5) * 20) // x
-        temp.push((Math.random() - 0.5) * 20) // y  
-        temp.push((Math.random() - 0.5) * 20) // z
-      }
-      return new Float32Array(temp)
-    }, [])
+    const temp = []
+    for (let i = 0; i < 300; i++) {
+      // Generate x, y, z coordinates (3 values per particle)
+      temp.push((Math.random() - 0.5) * 20) // x
+      temp.push((Math.random() - 0.5) * 20) // y  
+      temp.push((Math.random() - 0.5) * 20) // z
+    }
+    return new Float32Array(temp)
+  }, [])
 
 
   return (
     <>
-      <ambientLight intensity={0.16} />
+      <ambientLight intensity={0.1} />
 
-      <spotLight
-        position={[0, 5.33, 5.33]}
-        angle={0.3}
-        penumbra={0.5}
-        intensity={38.76}
-        castShadow
-        shadow-mapSize={[1024, 1024]}
-        color="#ffffff"
-        target-position={[0, 0, 0]}
-        transparent={true}
-      />
 
-   
+
 
       <Circle
-        ref={lightRef}  args={[10, 10]} position={[0, 0, -16]} transparent={true}>
-        <meshBasicMaterial color={"blue"} />
+        ref={lightRef} args={[5, 5]} position={[0, 0, -16]} transparent={true} scale={[1.13, 0.7, 1]}>
+        <meshBasicMaterial color={"white"} />
       </Circle>
 
       <Points positions={particles}>
@@ -57,37 +85,33 @@ export default function Scene() {
       </Points>
 
 
-      <mesh position={[0, 0, 0]} receiveShadow={true} rotation={[-1.57, 0, 0]}>
-        <planeGeometry args={[10, 10]} />
-        <meshPhysicalMaterial color="white" />
+
+
+      <mesh transparent castShadow={true} position={[-0.07, 1.37, 0]}>
+        <boxGeometry args={[2, 1, .01]} />
+         <primitive object={invertedAlphaMaterial} attach="material" />
       </mesh>
 
 
-      <Test />
 
-      <mesh position={[3, 0, 0]}>
-        <sphereGeometry args={[0.5]} />
-        <meshPhysicalMaterial color="blue" />
-      </mesh>
-
-       <EffectComposer multisampling={0}>
-        <Bloom 
+      <EffectComposer multisampling={0}>
+        <Bloom
           intensity={0.5}
           luminanceThreshold={0.9}
           luminanceSmoothing={0.9}
         />
         {lightRef.current && (
           <GodRays
-            sun={lightRef.current}
+            sun={lightRef}
             blendFunction={BlendFunction.Screen} // The blend function of this effect.
             samples={60} // The number of samples per pixel.
-            density={0.96} // The density of the light rays.
+            density={0.8} // The density of the light rays.
             decay={0.9} // An illumination decay factor.
             weight={0.4} // A light ray weight factor.
             exposure={0.6} // A constant attenuation coefficient.
             clampMax={1} // An upper bound for the saturation of the overall effect.
             //width={Resizer.AUTO_SIZE} // Render width.
-           // height={Resizer.AUTO_SIZE} // Render height.
+            // height={Resizer.AUTO_SIZE} // Render height.
             kernelSize={KernelSize.SMALL} // The blur kernel size. Has no effect if blur is disabled.
             blur={true} // Whether the god rays should be blurred to reduce artifacts.
           />
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,16 @@ import { OrbitControls } from "@react-three/drei";
 export default function Home() {
      return (
           <main className="w-screen h-screen">
-               <Canvas shadows>
+               <Canvas
+                    shadows
+                    gl={{
+                         antialias: true,
+                         alpha: false,
+                         preserveDrawingBuffer: false,
+                    }}
+                    camera={{ fov: 45 }}
+               >
+                    <color attach="background" args={["#000000"]} />
                     <OrbitControls />
                     <Scene />
                </Canvas>
